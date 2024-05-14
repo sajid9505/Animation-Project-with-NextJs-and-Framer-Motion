4cedf5718fab8671b9c37c46a81e4455a8088c04
sajid9505 <sajid.mahmud9505@gmail.com>
2024-05-15 00:36:55 +0600

    gallery centered

--- client/components/gallery/index.jsx
+++ client/components/gallery/index.jsx
@@ -1,24 +1,24 @@
 import styles from './style.module.scss'
 import Image from 'next/image';
 
-export default function index({handle}) {
+export default function index({ handle }) {
 
-  return (
-    <div className={styles.gallery}>
-        <div className={styles.imageContainer}>
-            <Image 
-                src={`/images/${handle}/background.jpg`}
-                alt="image"
-                fill
-            />
+    return (
+        <div className={styles.gallery}>
+            <div className={styles.imageContainer}>
+                <Image
+                    src={`/images/${handle}/background.jpg`}
+                    alt="image"
+                    fill
+                />
+            </div>
+            <div className={styles.vignette}>
+                <Image
+                    src={`/images/${handle}/1.jpg`}
+                    alt="image"
+                    fill
+                />
+            </div>
         </div>
-        <div className={styles.vignette}>
-          <Image 
-            src={`/images/${handle}/1.jpg`}
-            alt="image"
-            fill
-          />
-        </div>
-    </div>
-  )
+    )
 }
